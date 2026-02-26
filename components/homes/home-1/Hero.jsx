8ebd4b5e5f513e6ccea57e7016a6a46039fe7c2a
buqueslari <buqueslari@gmail.com>
2026-02-26 13:57:24 -0300

    update number

--- components/homes/home-1/Hero.jsx
+++ components/homes/home-1/Hero.jsx
@@ -5,7 +5,7 @@ import { slides } from "@/data/heroSlides";
 import Image from "next/image";
 export default function Hero() {
   function redirecionarParaWhatsApp() {
-    const telefone = "551151521970";
+    const telefone = "5511945769439";
     let mensagem = "Olá, Gaby! 😊 Gostaria de ver o catálogo, por favor.";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
