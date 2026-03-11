004b90eef148893de07e56f8471e2cd5d89ffc28
buqueslari <buqueslari@gmail.com>
2026-03-11 08:54:44 -0300

    update number

--- components/homes/home-1/Hero.jsx
+++ components/homes/home-1/Hero.jsx
@@ -5,7 +5,7 @@ import { slides } from "@/data/heroSlides";
 import Image from "next/image";
 export default function Hero() {
   function redirecionarParaWhatsApp() {
-    const telefone = "5511932416299";
+    const telefone = "5511953964832";
     let mensagem = "Olá, Gaby! 😊 Gostaria de ver o catálogo, por favor.";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "5511932416299";
+    const telefone = "5511953964832";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
