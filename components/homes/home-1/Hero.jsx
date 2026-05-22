9bb11688cb09cd563ddc3300a66bc5cabae469cc
buqueslari <buqueslari@gmail.com>
2026-05-22 19:05:13 -0300

    update number

--- components/homes/home-1/Hero.jsx
+++ components/homes/home-1/Hero.jsx
@@ -5,7 +5,7 @@ import { slides } from "@/data/heroSlides";
 import Image from "next/image";
 export default function Hero() {
   function redirecionarParaWhatsApp() {
-    const telefone = "5511917337761";
+    const telefone = "5511917338861";
     let mensagem = "Olá, Gaby! 😊 Gostaria de ver o catálogo, por favor.";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
--- components/productCards/ProductCard1.jsx
+++ components/productCards/ProductCard1.jsx
@@ -15,7 +15,7 @@ export default function ProductCard1({ product, gridClass = "" }) {
   }, [product]);
 
   function redirecionarParaWhatsApp() {
-    const telefone = "5511917337761";
+    const telefone = "5511917338861";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
