e93a741753ca6405f81775e14530d9bbc1f3ccc5
buqueslari <buqueslari@gmail.com>
2026-05-23 19:57:48 -0300

    tag google

--- app/layout.js
+++ app/layout.js
@@ -125,14 +125,14 @@ export default function RootLayout({ children }) {
         {/* Suas Flores Manager Script */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18000075591"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18148830321"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-18000075591');
+            gtag('config', 'AW-18148830321');
           `}
         </Script>
       </head>
